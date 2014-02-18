e0d159c67f95b4e64a1006a7a1c78083a7f3637d
Dagal <dejarding@gmail.com>
2014-02-18 21:35:52 +0100

    CGLLine : En cours de préparation

---
+++ DamierGL/src/CGL/CGLLine.cpp
@@ -0,0 +1,19 @@
+/*
+ * CGLLine.cpp
+ *
+ *  Created on: 18 févr. 2014
+ *      Author: dagal
+ */
+
+#include "CGLLine.h"
+
+CGLLine::CGLLine() : CGLObject()
+{
+
+}
+
+CGLLine::~CGLLine()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ DamierGL/src/CGL/CGLLine.h
@@ -0,0 +1,24 @@
+/*
+ * CGLLine.h
+ *
+ *  Created on: 18 févr. 2014
+ *      Author: dagal
+ */
+
+#ifndef CGLLINE_H_
+#define CGLLINE_H_
+
+#include "CGLObject.h"
+
+/*
+ *
+ */
+class CGLLine : public CGLObject
+{
+public:
+	CGLLine();
+	virtual
+	~CGLLine();
+};
+
+#endif /* CGLLINE_H_ */
