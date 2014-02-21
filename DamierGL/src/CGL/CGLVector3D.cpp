ab64a5763f5434ac55d5cc34a4c275478ec49060
Dagal <dejarding@gmail.com>
2014-02-21 21:16:55 +0100

    Vector3D updated and Motion deleted

--- DamierGL/src/CGL/CGLVector3D.cpp
+++ DamierGL/src/CGL/CGLVector3D.cpp
@@ -17,40 +17,54 @@ CGLVector3D::CGLVector3D()
 CGLVector3D::~CGLVector3D() {
 }
 
-double CGLVector3D::getX()
+double const& CGLVector3D::getX() const
 {
 	return x;
 }
 
-double CGLVector3D::getY()
+double const& CGLVector3D::getY() const
 {
 	return y;
 }
 
-double CGLVector3D::getZ()
+double const& CGLVector3D::getZ() const
 {
 	return z;
 }
 
-void CGLVector3D::set(double xv, double yv, double zv)
+void CGLVector3D::set(double const& xv, double const& yv, double const& zv)
 {
 	x = xv;
 	y = yv;
 	z = zv;
 }
 
-void CGLVector3D::setX(double val)
+void CGLVector3D::setX(double const& val)
 {
 	x = val;
 }
 
-void CGLVector3D::setY(double val)
+void CGLVector3D::setY(double const& val)
 {
 	y = val;
 }
 
-void CGLVector3D::setZ(double val)
+void CGLVector3D::setZ(double const& val)
 {
 	z = val;
 }
 
+bool CGLVector3D::isEqualTo(CGLVector3D const& b) const
+{
+	bool retour = false;
+	if (x==b.getX())
+		if (y==b.getY())
+			if (z==b.getZ())
+				retour = true;
+	return retour;
+}
+
+bool operator==(CGLVector3D const &a, CGLVector3D const &b)
+{
+	return a.isEqualTo(b);
+}
